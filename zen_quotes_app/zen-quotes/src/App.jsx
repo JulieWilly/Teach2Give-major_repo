195f7eff2145bd12d970ed744d53d9a284ae5814
Wilfred Muchire <logincoin40@gmail.com>
2024-06-06 05:39:09 +0300

    adding a refresh button to the code to show loading when a new request has been made.

--- zen_quotes_app/zen-quotes/src/App.jsx
+++ zen_quotes_app/zen-quotes/src/App.jsx
@@ -4,17 +4,23 @@ import Header from "./components/Header";
 import { GrNext } from "react-icons/gr";
 import Heros from "./pages/Heros";
 import './App.css'
+import { RxReload } from "react-icons/rx";
 import { RiDoubleQuotesL } from "react-icons/ri";
 import { RiDoubleQuotesR } from "react-icons/ri";
 const App = () => {
 const [quote, setQuote] = useState("");
+const [isLoading, setLoading ]= useState(false)
 
 const advices = async () => {
   try {
+    setLoading(true)
+
     const response = await fetch("https://api.adviceslip.com/advice")
     let adviceData = await response.json();
     // console.log(adviceData);
     setQuote(adviceData.slip.advice);
+    setLoading(false);
+
   } catch (error) {
     console.log(`this is the error ${error}`);
   }
@@ -33,10 +39,14 @@ useEffect(() => {
         <p className="advice-text">
           <RiDoubleQuotesL className="quotes" />
           {quote}
-
           <RiDoubleQuotesR className="quotes" />
         </p>
-        <button onClick={advices}>Next Quote<GrNext className="next"/></button>
+        {isLoading ? <p>{<RxReload className="reload" />}</p> : null}
+
+        <button onClick={advices}>
+          Next Quote
+          <GrNext className="next" />
+        </button>
       </div>
     </div>
   );
