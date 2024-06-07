18c0cc239cc5d6c96e00c29005373f5c99c74117
Wilfred Muchire <logincoin40@gmail.com>
2024-06-07 06:12:45 +0300

    repository structuring

---
+++ zen-quotes/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import "./headers.css";
+
+const Header = () => {
+  return (
+    <>
+      <h1 className="headerTitle">TODAYS QUOTE & ADVICE</h1>
+    </>
+  );
+};
+
+export default Header;
---
+++ zen-quotes/src/pages/Heros.jsx
@@ -0,0 +1,14 @@
+import "./heros.css";
+const Heros = () => {
+  return (
+    <>
+      <div className="heros">
+        <div className="right"></div>
+        <div className="left"></div>
+        <p className="dailyQuote"></p>
+      </div>
+    </>
+  );
+};
+
+export default Heros;
